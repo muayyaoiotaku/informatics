60af4a99e8d44c47c9465ebac96f8ff4a5239202
muayyaoiotaku <36034922+muayyaoiotaku@users.noreply.github.com>
2019-01-24 15:52:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'Pwr2qJRQrqfoIg40/sRZiJ1s7fG6ptpSuGHHP8UvgTlVyWPQDE1/+4bcMHEZvnHDRFVo2rQRY1OiG+iAxBuju2nNGpjhuRGtG1+RL4VBXBabK9qV8ZmnbijFGqgz4eGTIVOchyolgi23iyp3yfAO6wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
